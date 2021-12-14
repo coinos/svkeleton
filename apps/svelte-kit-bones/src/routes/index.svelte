035a075e78c60f21cc160559fec67e89035a5bb4
Derrick Schwabe <drschwabe@fastmail.com>
2021-12-13 20:49:43 -0700

    Add Svkeleton logo/favicon and some mild base theme

--- apps/svelte-kit-bones/src/routes/index.svelte
+++ apps/svelte-kit-bones/src/routes/index.svelte
@@ -1,6 +1,16 @@
 <script>
+  import Skull from "$components/Skull.svelte"
   import "../app.postcss"
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex items-center my-6">
+  <Skull></Skull>
+  <h1 class="text-3xl">Welcome to Svkeleton</h1>
+</div>
+
+<p class="ml-10">Visit <a href="https://github.com/satocean/svkeleton">github.com/satocean/svkeleton</a> to read the documentation</p>
+
+<style global>
+  body { @apply bg-black text-white }
+  a { @apply text-blue-100 no-underline font-bold }
+</style>
