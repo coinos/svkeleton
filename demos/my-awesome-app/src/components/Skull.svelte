e652157794c096f1e3b30d19bca6745acfd5a9d8
Derrick Schwabe <drschwabe@fastmail.com>
2021-12-19 12:41:45 -0700

    Add 'demos/my-awesome-app' to show a working docker Svkeleton example

---
+++ demos/my-awesome-app/src/components/Skull.svelte
@@ -0,0 +1,1 @@
+<img class="{$$props.class}" src="/favicon.png" />
---
+++ demos/my-awesome-app/src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script>
+  import Skull from "$components/Skull.svelte"
+  import "../app.postcss"
+</script>
+
+<div class="flex items-center my-6">
+  <Skull></Skull>
+  <h1 class="text-3xl">Welcome to My Awesome App!</h1>
+</div>
+
+<p class="ml-10">Visit <a href="https://github.com/coinos/svkeleton">github.com/coinos/svkeleton</a> to read the documentation</p>
+
+<style global>
+  body { @apply bg-black text-white }
+  a { @apply text-blue-100 no-underline font-bold }
+</style>
